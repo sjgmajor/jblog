8b866aa93b68fd77e5d21ea0eebffdd8a40855ee
서정권 <sjg3234@gmail.com>
2023-10-06 21:16:06 +0900

    jblog03 #finish

--- schema/jblog.sql
+++ schema/jblog.sql
@@ -14,31 +14,45 @@ select * from blog;
 select * from category;
 select * from post;
 
-
+select count(b.no) as postCount, a.no, a.name, a.description from category a 
+LEFT JOIN post b ON a.no = b.category_no
+where a.blog_id = 'sjg3234'
+group by a.no;
 
 select a.title, a.contents, a.category_no, b.name, b.blog_id from post a, category b
 where a.category_no = b.no
 and blog_id = (select a.blog_id from category a where a.no = 19)
 and a.category_no = 19;
 
+select a.title, a.contents
+					  from post a
+					  where a.no = 8;
+
+
 select a.category_no, a.title, a.contents
 from post a
-where a.category_no =
+where a.category_no = 19 order by a.no asc limit 1;
 (select min(b.no) 
 from category b
 where b.blog_id = 'sjg3234'
-and exists(select 1 from post c where c.category_no = b.no));
+and exists(select 1 from post c where c.category_no = b.no))
+order by a.no asc limit 1;
+
+select b.no
+from category b
+where b.blog_id = 'sjg3234'
+and exists(select 1 from post c where c.category_no = b.no);
 
 select a.title, a.contents
 						  from post a
-						 where a.no = 7;
+						 where a.no = 5;
                          
 select MIN(a.no)
 from post b
 where exist(b.category_no = (select MIN(a.no) from category a where blog_id = 'sjg3234');
 
-WHERE EXISTS (select a.no from category a where blog_id = 'sjg3234'
-order by a.no asc limit 1)
+
+
 
 select a.no from category a where blog_id = 'sjg3234'
 order by a.no asc limit 1;
@@ -58,13 +72,21 @@ select name, id, password
 insert into blog value(concat('sjg3234','의 jblog'), '/assets/images/spring-logo.jpg', 'sjg3234');
 
 insert into category value( null, '미분류', '카테고리를 정하지 않음', 'sjg3234');
-select name, description from category where blog_id = 'sjg3234';
+
+select no from category where blog_id = 'sjg3234';
+
+select count(b.no) as postCount, a.no, a.name, a.description from category a, post b
+where a.no = b.category_no
+and a.blog_id = 'sjg3234'
+group by a.no;
+
 select title, image, blog_id as blogId
 		  	  from blog
 			 where blog_id='sjg3234';
              
- delete from category where no = 11 and blog_id = 'sjg3234';            
-             
+ delete a, b from category a join post b on a.no = b.category_no where a.no = 12 and a.blog_id = 'sjg3234';
+
+
              update blog
 			   set title = 'sjg3234',
 				   image = '/assets/images/spring-logo.jpg'
